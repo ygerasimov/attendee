01ed63998a1401d0bcc5e0658fb182f4865dc4f3
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2018-06-23 00:12:00 +0300

    Add List Builder.

---
+++ src/AttendeeListBuilder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\attendee;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityListBuilder;
+
+class AttendeeListBuilder extends EntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $rows['name'] = $this->t('Name');
+    $rows['seniority'] = $this->t('Seniority');
+    $rows['bio'] = $this->t('Bio');
+
+    return $rows + parent::buildHeader();;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    $row['name'] = $entity->name->value;
+    $row['seniority'] = $entity->seniority->value;
+    $bio = $entity->bio->entity;
+    $row['bio']['data'] = [
+      '#type' => 'link',
+      '#title' => $bio->label(),
+      '#url' => $bio->toUrl(),
+    ];
+
+    return $row + parent:: buildRow($entity);;
+  }
+
+}
--- src/Entity/Attendee.php
+++ src/Entity/Attendee.php
@@ -3,6 +3,9 @@
 namespace Drupal\attendee\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+
 
 /**
  * Defines the Attendee entity type.
@@ -17,7 +20,7 @@ use Drupal\Core\Entity\ContentEntityBase;
  *       "edit" = "Drupal\Core\Entity\ContentEntityForm",
  *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm",
  *     },
- *     "list_builder" = "Drupal\Core\Entity\EntityListBuilder",
+ *     "list_builder" = "Drupal\attendee\AttendeeListBuilder",
  *   },
  *   base_table = "attendee",
  *   data_table = "attendee_field_data",
@@ -36,5 +39,68 @@ use Drupal\Core\Entity\ContentEntityBase;
  * )
  */
 class Attendee extends ContentEntityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Name'))
+      ->setDescription(t('First and last name of attendee.'))
+      ->setRequired(TRUE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -5,
+      ]);
+
+    $fields['seniority'] = BaseFieldDefinition::create('list_string')
+      ->setLabel(t('Seniority'))
+      ->setDescription(t('How fluent developer you are.'))
+      ->setRequired(TRUE)
+      ->setSetting('allowed_values', static::getSeniorityOptions())
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'options_select',
+      ]);
+
+    $fields['bio'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Bio'))
+      ->setDescription(t('Attendee Bio page'))
+      ->setSetting('target_type', 'node')
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'settings' => [
+          'placeholder' => t('Select if you created dedicated Bio page'),
+        ],
+      ]);
+
+    return $fields;
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function label() {
+    return $this->name->value;
+  }
+
+  /**
+   * Options for Seniority levels.
+   */
+  static function getSeniorityOptions() {
+    // Do queries to database.
+    return [
+      'junior' => t('Junior'),
+      'middle' => t('Middle'),
+      'senior' => t('Senior'),
+      'lead' => t('Team Lead'),
+      'cto' => t('CTO'),
+    ];
+  }
 
 }
